993da9b1be88b2035d08a5ae182ca4d102861156
jclemons555 <83421930+jclemons555@users.noreply.github.com>
2024-04-08 13:00:01 -0400

    Update index.js

---
+++ src/components/surveyData/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import SurveyDashboardComponent from "./SurveyDashboardComponent";
+
+const root = createRoot(document.getElementById("surveyDashboardComponent"));
+root.render(<SurveyDashboardComponent />);
