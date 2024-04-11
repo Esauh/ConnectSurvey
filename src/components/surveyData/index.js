6cf6d2b9b7913a4db9f0a6b40367416c4ff73cd9
jclemons555 <83421930+jclemons555@users.noreply.github.com>
2024-04-11 14:23:30 -0400

    Update index.js

--- src/components/surveyData/index.js
+++ src/components/surveyData/index.js
@@ -1,23 +1,6 @@
-const dataUrl = "https://api.surveyjs.io/private/Surveys/getSurveyPublicResults/a8fbb673-2519-4e3b-85ef-08429c15d9ff";
-class SurveyDashboardComponent extends React.Component {
-    componentDidMount() {
-        const survey = new Survey.Model(json);
-        fetch(dataUrl).then(response => response.json()).then(data => {
-            const dataFromServer = data.Data;
-            const vizPanel = new SurveyAnalytics.VisualizationPanel(
-                survey.getAllQuestions(),
-                dataFromServer
-            );
-            
-            
-            document.getElementById("loadingIndicator").style.display = "none";
-            vizPanel.render("surveyDashboardContainer");
-        });
-    }
-    render() {
-        return React.createElement("div", { id: "surveyDashboardContainer" });
-    }
-}
+import React from "react";
+import { createRoot } from "react-dom/client";
+import SurveyDashboardComponent from "./SurveyDashboardComponent";
 
-const root = ReactDOM.createRoot(document.getElementById("surveyDashboardComponent"));
+const root = createRoot(document.getElementById("surveyDashboardComponent"));
 root.render(<SurveyDashboardComponent />);
