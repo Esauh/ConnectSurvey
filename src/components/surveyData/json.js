8a08e8b834547340fa39923c02c6817833c73776
jclemons555 <83421930+jclemons555@users.noreply.github.com>
2024-04-11 12:29:02 -0400

    Update json.js

--- src/components/surveyData/json.js
+++ src/components/surveyData/json.js
@@ -1,6 +1,1 @@
-export const json = { 
-    "surveyId": "a8fbb673-2519-4e3b-85ef-08429c15d9ff",
-    "surveyPostId": "0d54c18b-f977-4e36-a35b-a6474253a4b1",
-    "surveyShowDataSaving": true
-};
-
+export const json = { "surveyId": "a8fbb673-2519-4e3b-85ef-08429c15d9ff" };
